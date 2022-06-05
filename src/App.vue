e3b892235d94cbc5a4fae28ef5a4fe9cc87e7277
zhangzihan <2863065234@qq.com>
2022-06-05 20:03:50 +0800

    翻转世界

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,27 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+<h1>{{ msg }}</h1>
+<button @click="btn">翻转世界</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+data() {
+      return {
+          msg:'Hello,world'
+      }
+},
+methods:{
+  btn() {
+    let arr=this.msg.split("")
+    arr.reverse()
+    this.msg=arr.join("")
   }
+}
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
